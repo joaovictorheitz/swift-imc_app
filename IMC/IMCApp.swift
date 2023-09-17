de0899e5b167c672a0421f8c9e091e9b7704bd01
joaovictorheitz <vcitorsguedes@hotmail.com>
2023-09-16 23:14:34 -0300

    Initial Commit

---
+++ IMC/IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCApp.swift
+//  IMC
+//
+//  Created by João Victor de Souza Guedes on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
